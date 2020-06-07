6d203a621ae3c3b8f6defab4a5a130f900269d2f
Rojen <rojenrosal@gmail.com>
2020-06-08 03:50:28 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const container = document.querySelector('.container')
+const text = document.querySelector('#text')
+
+const totalTime = 7500
+const breatheTime =  (totalTime / 5) * 2
+const holdTime = totalTime/5 
+
+breatheAnimation()
+
+function breatheAnimation (){
+    text.innerHTML = "INHALE!"
+    container.className = 'container grow'
+
+    setTimeout(()=> {
+        text.innerText = 'HOOOOOLD!'
+
+        setTimeout(() => {
+            text.innerText = 'EXHALE!'
+            container.className = 'container shrink'
+
+        }, holdTime)
+    }, breatheTime)
+}
+
+setInterval(breatheAnimation, totalTime)
